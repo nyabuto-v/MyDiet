02b472dc1ab39978638764c0e5ecfd17ae00cbad
nyabuto-v <nyabutovice@gmail.com>
2018-06-20 17:18:35 +0300

    modify layout in the second activity

--- app/src/main/java/com/app/honpaul/mydiet/DietActivity.java
+++ app/src/main/java/com/app/honpaul/mydiet/DietActivity.java
@@ -1,13 +1,20 @@
 package com.app.honpaul.mydiet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DietActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Find the Diet for Chronic Diseases: " + location);
     }
 }
