25dd0e69244fb25c87d5df3be9da2728ef7eee17
nyabuto-v <nyabutovice@gmail.com>
2018-06-20 16:41:06 +0300

    add the second activity

--- app/src/main/java/com/app/honpaul/mydiet/MainActivity.java
+++ app/src/main/java/com/app/honpaul/mydiet/MainActivity.java
@@ -1,13 +1,26 @@
 package com.app.honpaul.mydiet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindDietButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindDietButton = (Button) findViewById(R.id.findDietButton);
+        mFindDietButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DietActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
