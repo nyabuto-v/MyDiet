6a43518f61608ca75ff95a2fbafb5fd047510c11
nyabuto-v <nyabutovice@gmail.com>
2018-06-21 10:43:04 +0300

    Implemented custom fonts in the main activity layout

--- app/src/main/java/com/app/honpaul/mydiet/DietActivity.java
+++ app/src/main/java/com/app/honpaul/mydiet/DietActivity.java
@@ -3,18 +3,24 @@ package com.app.honpaul.mydiet;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class DietActivity extends AppCompatActivity {
-    private TextView mLocationTextView;
+    private TextView mDiseaseTextView;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet);
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        mDiseaseTextView = (TextView) findViewById(R.id.diseaseTextView);
         Intent intent = getIntent();
-        String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Find the Diet for Chronic Diseases: " + location);
+        String disease = intent.getStringExtra("disease");
+        mDiseaseTextView.setText("Find the Diet for Chronic Disease: " + disease);
+//        mListView = (ListView) findViewById(R.id.listView);
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, diet);
+//        mListView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/app/honpaul/mydiet/MainActivity.java
+++ app/src/main/java/com/app/honpaul/mydiet/MainActivity.java
@@ -1,31 +1,38 @@
 package com.app.honpaul.mydiet;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindDietButton;
-    private EditText mLocationEditText;
+    private EditText mDiseaseEditText;
+    private TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mDiseaseEditText = (EditText) findViewById(R.id.diseaseEditText);
         mFindDietButton = (Button) findViewById(R.id.findDietButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
         mFindDietButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
+                String disease = mDiseaseEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, DietActivity.class);
-                intent.putExtra("location", location);
+                intent.putExtra("disease", disease);
                 startActivity(intent);
             }
         });
